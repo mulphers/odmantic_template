cb946c2e62eea32e281441586b791dd68d1d6176
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-05-09 14:54:59 +0300

    add project configuration

---
+++ src/core/settings.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='./.env',
+        env_file_encoding='utf-8',
+        case_sensitive=False,
+        extra='ignore',
+        env_prefix='database_'
+    )
+
+    host: str
+    port: int
+
+    @property
+    def url(self):
+        return f'mongodb://{self.host}:{self.port}/'
+
+
+class Settings(BaseSettings):
+    db: DatabaseSettings
+
+    @staticmethod
+    def root_dir() -> Path:
+        return Path(__file__).resolve().parent.parent.parent
+
+
+def load_settings(
+        database_settings: Optional[DatabaseSettings] = None
+) -> Settings:
+    return Settings(
+        db=database_settings or DatabaseSettings()  # type: ignore[call-arg]
+    )
