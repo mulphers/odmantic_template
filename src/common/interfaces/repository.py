c4fbb2242231588f0d470d66532e25555eb3cd58
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-05-10 14:06:26 +0300

    add abstract repository

---
+++ src/common/interfaces/repository.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from typing import Any, Generic, Optional, Sequence, Type
+
+from src.common.types import ColumnType, EntryType, SessionType
+
+
+class AbstractRepository(ABC, Generic[EntryType, SessionType, ColumnType]):
+    model: Type[EntryType]
+
+    def __init__(self, session: SessionType) -> None:
+        self.session = session
+
+    @abstractmethod
+    async def create(self, data: dict[str, Any]) -> Optional[EntryType]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def select(self, *clauses: ColumnType) -> Optional[EntryType]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def select_many(
+            self,
+            *clauses: ColumnType,
+            offset: Optional[int] = None,
+            limit: Optional[int] = None
+    ) -> Sequence[EntryType]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def update(self, *clauses: ColumnType, data: dict[str, Any]) -> Optional[EntryType]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def delete(self, *clauses: ColumnType) -> Sequence[EntryType]:
+        raise NotImplementedError
