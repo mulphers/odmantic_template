76777a8776c30acd93fa9bd52fde6f047028cc57
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-05-10 14:04:51 +0300

    add types

--- src/common/types.py
+++ src/common/types.py
@@ -1,4 +1,9 @@
 from typing import TypeVar
 
+ModelType = TypeVar('ModelType')
+
 SessionType = TypeVar("SessionType")
 TransactionType = TypeVar("TransactionType")
+
+EntryType = TypeVar("EntryType")
+ColumnType = TypeVar("ColumnType")
