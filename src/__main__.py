762306d23c2b979b8a4dbb0d9460e59f48c0e340
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-05-26 18:09:30 +0300

    add di

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,22 @@
 import asyncio
 
+from fast_depends import dependency_provider
+
+from src.common.markers import TransactionGatewayMarker
+from src.core.settings import load_settings
+from src.database import TransactionGateway
+from src.database.core.connection import create_engine
+
 
 async def main() -> None:
-    pass
+    settings = load_settings()
+
+    database_engine = create_engine(
+        host=settings.db.host,
+        port=settings.db.port
+    )
+
+    dependency_provider.override(TransactionGatewayMarker, TransactionGateway(database_engine))
 
 
 if __name__ == '__main__':
